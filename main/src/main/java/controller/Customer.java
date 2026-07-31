9d0798fbf7bd075a4f3a61b7b60da9ad659e9dfe
nguyendang2909 <nguyendang2909@gmail.com>
2026-07-31 13:46:03 +0700

    Add a constructor to the customer class

--- main/src/main/java/controller/Customer.java
+++ main/src/main/java/controller/Customer.java
@@ -1,7 +1,15 @@
 package controller;
 
 public class Customer {
-    
+    private static int idCounter = 1;
+    private int id;
+    private boolean arrivalState = false;
+    private long arrivalTime;
+    private long departureTime;
+    public Customer() {
+        this.id = idCounter++;
+        this.arrivalState = arrivalState;
+    }
 }
 
 // TO DO: id, state, arrivalTime, departureTime
